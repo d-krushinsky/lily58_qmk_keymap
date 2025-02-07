b531529c450cffb34913a2f1762b614967cd9780
Dmitry Krushinsky <d.krushinsky.00@gmail.com>
2025-02-07 10:35:17 +0100

    Add multimedia keys

--- keymap.c
+++ keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_TRNS,
       KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,                        KC_EQL , KC_LBRC, KC_RBRC, KC_NO  , KC_NO  , KC_NO  ,
       KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_BRIU, KC_BRID,                        KC_PLUS, KC_LCBR, KC_RCBR, KC_NO  , KC_NO  , KC_NO  ,
-      KC_TRNS, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_TILD,      KC_DEL , KC_PIPE, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+      KC_TRNS, KC_NO  , KC_NO  , KC_MPLY, KC_MPRV, KC_MNXT, KC_TILD,      KC_DEL , KC_PIPE, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, MO(3)  , KC_TRNS, KC_TRNS
     ),
     [_RAISE] = LAYOUT(
